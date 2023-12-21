060660f1e50f2c1c2d91ff829b1c155759cf288f
DoanHuuThanh <thanh022302@gmail.com>
2023-12-21 17:44:48 +0700

    refs #1 DEV:write test by rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,5 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'factory_bot_rails'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
@@ -62,4 +63,13 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+FactoryBot::SyntaxRunner.class_eval do
+  include RSpec::Mocks::ExampleMethods
+end
+
+RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+  # Other configurations...
+end
 end
